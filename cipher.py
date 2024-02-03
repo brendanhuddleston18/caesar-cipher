3f55283f77f5e8431864d71dc5dedd0e2e9d6216
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-02-03 11:46:47 -0600

    added more conditions to cracked function

--- cipher.py
+++ cipher.py
@@ -5,7 +5,6 @@ from caesar_cipher.corpus_loader import word_list, name_list
 def encrypt(plain, shift):
   encrypted_text = ""
   number_of_characters = 26
-  # normalized_text = plain
 
   for char in plain:
     if char.isalpha():
@@ -23,8 +22,6 @@ def encrypt(plain, shift):
       encrypted_text += char
 
   return encrypted_text
-  
-# print(ord(" "))
 
 def decrypt(encrypted_text, shift):
   return encrypt(encrypted_text, -shift)
@@ -33,20 +30,22 @@ def crack(encrypted_text):
   shift= 0
   word_count = 0
   cracked_text = ""
-  for i in range(65):
+  for i in range(26):
     decrypted_subject = decrypt(encrypted_text, i)
-    words = re.sub(r'[^A-Za-z]+',' ', decrypted_subject)
-    shift += 1
-    print(words)
+    words = re.sub(r'[^A-Za-z]+',' ', decrypted_subject).split()
+    current_word_count = 0
     for word in words:
       if word.lower() in word_list or word in name_list:
-        word_count += 1
-        print(word_count)
-    # if decrypted_word.lower() in word_list or decrypted_word in name_list:
-    #   cracked_text += decrypted_word
-    #   print(word_count)
-  
-    # for decrypted_word in words:
+       current_word_count += 1
+
+    if current_word_count > word_count:
+      word_count = current_word_count
+      cracked_text = decrypted_subject
+
+  if (word_count > len(words)) / 2:
+    return cracked_text
+  else:
+    return ''
 
 if __name__ == "__main__":
   sample = "AAAA"
